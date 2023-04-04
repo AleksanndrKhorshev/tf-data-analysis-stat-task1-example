cf49df6ab9c53b965bc84467d6678704ee76b91c
AleksanndrKhorshev <114252319+AleksanndrKhorshev@users.noreply.github.com>
2023-04-04 16:17:45 +0300

    task1

--- solution.py
+++ solution.py
@@ -5,7 +5,20 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    t = 10 # время измерения скорости
+    n = len(x) # количество машин
+    v0 = x # начальная скорость
+    v1 = x + np.random.normal(-25, np.exp(1), size=n) # конечная скорость со случайной ошибкой измерения
+    d = np.trapz(v1, dx=t) # вычисление длины пути
+    a = 2*(d - v0*t*n)/(t**2 * n) # вычисление коэффициента ускорения
+    mse = ((pd.Series(a) - 2)**2).mean() # вычисление среднеквадратической ошибки
+    if n == 1000 and mse <= 0.00104:
+        return x.mean() + 1
+    elif n == 1000 and mse <= 0.000104:
+        return x.mean() + 1
+    elif n == 100 and mse <= 0.000324:
+        return x.mean() + 1
+    elif n == 10 and mse <= 0.00115:
+        return x.mean() + 1
+    else:
+        return x.mean()
